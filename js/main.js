d9b5fed2608055a40e96d294fa6c45f68a517b85
summercrane <jjaskulis@gmail.com>
2018-01-12 21:04:55 +0200

    1

--- js/main.js
+++ js/main.js
@@ -3,60 +3,67 @@
 var app = new Vue({
     el: "#app-2048",
     data: {
+        active:true,
         gameboard: [
             [{
-                value: "4",
-                merged: false
+                value: "",
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }],
             [{
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }],
             [{
-                value: "2",
-                merged: false
+                value: "",
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }],
             [{
-                value: "2",
-                merged: false
+                value: "",
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }, {
                 value: "",
-                merged: false
+                mergeNum : 0
             }]
-        ]
+        ],
+        lastTurn: {
+            lastMove:"",
+            hasMerged:false,
+            hasMoved:false
+        },
+        gameOver: false
     },
     methods: {
         newTile: function (amount) {
@@ -71,29 +78,35 @@ var app = new Vue({
                 this.newTile(amount - 1);
             }
         },
-        makeMove: function (direction) {
-            switch (direction) {
-                case "right":
-                    this.mergeTile("rows", "backward");
-                    this.moveTile("rows", "backward");
-                    break;
-                case "left":
-                    this.mergeTile("rows", "forward");
-                    this.moveTile("rows", "forward");
-                    break;
-                case "down":
-                    this.mergeTile("collumns", "backward");
-                    this.moveTile("collumns", "backward");
-                    break;
-                case "up":
-                    this.mergeTile("collumns", "forward");
-                    this.moveTile("collumns", "forward");
-                    break;
+        makeMove: function (key) {
+            if (this.lastTurn.lastMove == "" || key !== this.lastTurn.lastMove) {
+                switch (key) {
+                    case "right":
+                        this.mergeTile("rows", "backward");
+                        this.moveTile("rows", "backward");
+                        break;
+                    case "left":
+                        this.mergeTile("rows", "forward");
+                        this.moveTile("rows", "forward");
+                        break;
+                    case "down":
+                        this.mergeTile("collumns", "backward");
+                        this.moveTile("collumns", "backward");
+                        break;
+                    case "up":
+                        this.mergeTile("collumns", "forward");
+                        this.moveTile("collumns", "forward");
+                        break;
+                }
+                this.lastKey = key;
+                if(this.lastTurn.hasMoved || this.lastTurn.hasMerged){
+                this.newTile();
             }
-            this.newTile()
+            }
         },
         mergeTile: function (dimension, direction) {
             let i, j, increment, loopEnd;
+            this.lastTurn.hasMerged = false;
             for (let x = 0; x <= 3; x++) {
                 if (direction == "backward") {
                     j = 3;
@@ -112,10 +125,17 @@ var app = new Vue({
                     let tileB = dimension == "rows" ? app.gameboard[x][i].value : app.gameboard[i][x].value;
 
                     if ((tileA == tileB) && (tileA > 0 && tileB > 0)) {
+                        dimension == "rows" ? (app.gameboard[x][j].mergeNum++) : (app.gameboard[j][x].mergeNum++);
                         dimension == "rows" ? (app.gameboard[x][j].value = tileA * 2) : (app.gameboard[j][x].value = tileA * 2);
+                        
+                        dimension == "rows" ? (app.gameboard[x][i].mergeNum--) : (app.gameboard[x][i].mergeNum--);                        
                         dimension == "rows" ? (app.gameboard[x][i].value = "") : (app.gameboard[i][x].value = "");
+                        this.lastTurn.hasMerged = true;
                         break;
-                    } else {
+                    } else if (tileA > 0 && tileB <= 0) {
+                        i += increment;
+                    }
+                    else {
                         j += increment;
                         i += increment;
                     }
@@ -123,37 +143,42 @@ var app = new Vue({
             }
         },
         moveTile: function (dimension, direction) {
-            // let j,
-            //     increment,
-            //     loopEnd;
-            // let i = 0;
-            // for (let x = 0; x <= 3; x++) {
-            //     if (direction == "backward") {
-            //         j = 2;
-            //         increment = -1;
-            //         loopEnd = 3;
-            //     } else if (direction == "forward") {
-            //         j = 1;
-            //         increment = 1;
-            //         loopEnd = 0;
-            //     }
-            //     for (let y = 0; y <= 2; y++) {
-            //         let tileA = dimension == "rows" ? app.gameboard[x][j].value : app.gameboard[j][x].value;
-            //         if (tileA) {
-            //             while (j + i != loopEnd) {
-            //                 let tileB = dimension == "rows" ? app.gameboard[x][j + i].value : app.gameboard[j + i][x].value;
-            //                 if ((tileB == tileB) && (tileA > 0 && tileB > 0)) {
-            //                     dimension == "rows" ? (app.gameboard[x][j].value = tileA * 2) : (app.gameboard[j][x].value = tileA * 2);
-            //                     dimension == "rows" ? (app.gameboard[x][i].value = "") : (app.gameboard[i][x].value = "");
-            //                     break;
-            //                 } else {
-            //                     i += increment;
-            //                 }
-            //             }
-            //         }
-            //         j += increment;
-            //     }
-            // }
+            let j,
+                increment,
+                loopEnd,
+                i;
+            this.lastTurn.hasMoved = false;    
+            for (let x = 0; x <= 3; x++) {
+                if (direction == "backward") {
+                    j = 2;
+                    increment = -1;
+                    loopEnd = 4;
+                } else if (direction == "forward") {
+                    j = 1;
+                    increment = 1;
+                    loopEnd = -1;
+                }
+                for (let y = 0; y <= 2; y++) {
+                    let tileA = dimension == "rows" ? app.gameboard[x][j].value : app.gameboard[j][x].value;
+                    i = j - increment;
+                    if (tileA) {
+                        while (i != loopEnd) {
+                            let tileB = dimension == "rows" ? app.gameboard[x][i].value : app.gameboard[i][x].value;
+                            if (tileB) {
+                                break;
+                            }
+                            i -= increment;
+                        }
+                        i += increment;
+                        if(i!=j){
+                            dimension == "rows" ? (app.gameboard[x][j].value = "") : (app.gameboard[j][x].value = "");
+                            dimension == "rows" ? (app.gameboard[x][i].value = tileA) : (app.gameboard[i][x].value = tileA);
+                            this.lastTurn.hasMoved = true;
+                        }
+                    }
+                    j += increment;
+                }
+            }
         }
     }
 })
